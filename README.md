d70ebdfbc2bf6d30884035035953ba708b93e9e1
Sarah Hvid Andersen <shandersen@hotmail.com>
2022-05-26 11:29:34 +0200

    upload files

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ The purpose of this assignment is to perform a network analysis. The ```.py``` c
 The full assignment description is available in the ```assignment3.md``` file.
 
 ## Methods
-Initially, the user must specify either a CSV file or directory of CSV files as input (*note* these should be tab delimited). The same steps are then applied for each file specified, regardless of the input. The file is loaded as a ```pandas``` dataframe. The ```Networkx``` module is then used to conduct the network analysis itself. Three centrality scores are calculated and saved in a CSV file; degree centrality, eigen vector centrality and betweenness centrality. The CSV file will be saved in the ```output``` folder, and the name will vary depending on the input file name. A plot will also be created of the network graph. The user may specify a plotting style to use: *kamada_kawai*, *circular* or *spring*. The standard plot is the default value. The plot will be saved in the ```output``` folder and named according to plot style and input file name. 
+Initially, the user must specify either a CSV file or directory of CSV files as input (*note* these should be tab delimited). The same steps are then applied for each file specified, regardless of the input type. The file is loaded as a ```pandas``` dataframe. The ```Networkx``` module is then used to conduct the network analysis itself. Three centrality scores are calculated and saved in a CSV file; degree centrality, eigen vector centrality and betweenness centrality. The CSV file will be saved in the ```output``` folder, and the name will vary depending on the input file name. A plot will also be created of the network graph. The user may specify the following parameters:
+- layout (-l): *kamada_kawai*, *circular*, *spring* or *random*. *Spring* is the default.
+- node_size (-n): boolean input, **0** makes all nodes the same size, **1** bases the nodesize on the number of degrees a node has. **0** is the default.
+- edge_width (-e): boolean input, **0** makes all edges the same width, **1** bases the width on the weight between two nodes. **0** is the default.\
+
+A scaling function is used to fit the values of degree and weight within a specified range. The plot will be saved in the ```output``` folder and named according to input file name, layout, nodesize value and edge width value. 
 
 ## Usage
 In order to run the script, certain modules need to be installed. These are available in the ```requirements.txt``` file. The folder structure must be the same as in this GitHub repository (ideally, clone the repository).
@@ -26,13 +31,13 @@ Specified file:
 ```bash
 python src/network_analysis.py -f data/H5.csv
 ```
-Specified file and plot style:
+Specified file and layout:
 ```bash
-python src/network_analysis.py -f data/H5.csv -p kamada_kawai
+python src/network_analysis.py -f data/H5.csv -l kamada_kawai
 ```
-Specified directory and plot style:
+Specified directory, layout, node_size and edge_width:
 ```bash
-python src/network_analysis.py -f data -p circular
+python src/network_analysis.py -f data -l circular -n 1 -e 1
 ```
 
 Examples of the outputs of the script can be found in the ```output``` folder. 
@@ -41,7 +46,7 @@ Examples of the outputs of the script can be found in the ```output``` folder.
 The results of the script are as expected. The plots and CSV files are created as they should. As this script does not attempt to analyse any particular data but rather functions on any _'random'_ data, there are no results to discuss per se.\
 The output is the result:\
 \
-[**CSV file**](/output/network_H8.csv)
+[**CSV file**](/output/H8_network.csv)
 
 | Name  | Degree | Eigenvector  | Betweenness |
 | ------------- | ------------- | ------------- | ------------- |
@@ -52,11 +57,20 @@ The output is the result:\
 
 **Standard plot**
 
-<img src="/output/nx_standard_H5.png" width="500" height="400">
+<img src="/output/KJ_spring_0_0.png" width="500" height="400">
 
-**Circular plot**
+**Circular plot, nodesize**
 
-<img src="/output/nx_circular_H5.png" width="500" height="400">
+<img src="/output/H8_circular_True_0.png" width="500" height="400">
+
+**Kamada_kawai, nodesize and edgewidth**
+
+<img src="/output/KJ_kamada_kawai_True_True.png" width="500" height="400">
+
+**Random, edgewidth**
+
+<img src="/output/H8_random_0_True.png" width="500" height="400">
+
 
 
 
--- src/network_analysis.py
+++ src/network_analysis.py
@@ -4,11 +4,14 @@ A script that performs a simple network analysis on any undirected, weighted edg
 
 # System tools
 import os
+import sys
 import glob
 import argparse
 
 # Data analysis
 import pandas as pd
+import math
+import numpy as np
 from collections import defaultdict
 
 # Network analysis tools
@@ -18,23 +21,24 @@ import matplotlib.pyplot as plt
 
 def parse_args():
     '''
-    Function that specifies the required and available arguments.
+    Function that specifies the available arguments
     '''
     # Initialise argparse
     ap = argparse.ArgumentParser()
     
     # command line parameters
     ap.add_argument("-f", "--file_input", required = True, help = "The filename or directory we want to work with")
-    ap.add_argument("-p", "--plot_style", required = False, default = 'standard', help = "The style of the plot to be created by networkx (kamada_kawai, circular, spring)")
+    ap.add_argument("-l", "--layout", required = False, default = 'spring', help = "The layout of the plot to be created by networkx (spring, kamada_kawai, circular, random)")
+    ap.add_argument("-node", "--node_size", required = False, default = 0, type = bool, help = "specifies whether the size of the nodes should be based on the degree of the node")
+    ap.add_argument("-edge", "--edge_width", required = False, default = 0, type = bool, help = "specifies whether the width of the edges should be based on the weight of the connection")
 
     args = vars(ap.parse_args())
     return args
 
 
-# function that loads the data
 def read_df(filepath):
     '''
-    Function that reads in a CSV file that is tab separated. It also returns the cleaned name of the file.
+    Function that reads in a CSV file that's tab separated. it also returns the cleaned name of the file.
     
     filepath: the path to the CSV file.
     '''
@@ -45,13 +49,25 @@ def read_df(filepath):
     file_only = split_filename.split(".")[0]
     
     return df, file_only
+
+
+def scale(x, a, b):
+    '''
+    Function that contains a formulat that scales an array of values to a range between a and b.
+    
+    x: array to be scaled
+    a: the minimum value of the new scale
+    b: the maximum value of the new scale
+    '''
+    xnormalized = ((b - a) * ((x - min(x)) / (max(x) - min(x))) + a)  
+    return xnormalized
        
 
 def network_analysis(df):
     '''
     Function that performs a network analysis on an edgelist. The graph object is returned along with the degree, eigenvector and betweenness centrality scores.
     
-    df: dataframe of an edgelist with columns source, target and weight.
+    df: dataframe of an edgelist with columns: source, target and weight.
     '''
     # create network
     G = nx.from_pandas_edgelist(df, "Source", "Target", ["Weight"])
@@ -64,9 +80,77 @@ def network_analysis(df):
     return G, dg, ev, bc
 
 
+def plot_network(G, df, file_name):
+    '''
+    Function that plots a network graph in different styles and saves it with the filename the graph was created from. Dependent on user input the nodesizes can vary by degree and the edge width can vary by weight.
+    
+    G: a networkx graph object
+    file_name: the filename we are working with
+    '''
+    # grabbing plot argument
+    args = parse_args()
+    plot_style = args['layout']
+    n = args['node_size']
+    e = args['edge_width']
+    
+    # calculate degree values for the node size 
+    dg = dict(G.degree())
+    node_sizes = []
+    for k,v in dg.items():
+        node_sizes.append(v)
+
+    # scale values
+    node_sizes = np.array(node_sizes)
+    node_sizes = scale(node_sizes, 1200, 3500)
+
+    # get weight for edge width
+    weight = df['Weight'].values
+    weight = scale(weight, 0.6, 7)
+    
+    # setting standard values if the user input is 0
+    if args['node_size'] == 0:
+        node_sizes = 2000
+        
+    if args['edge_width'] == 0:
+        weight = 0.75
+        
+    f = plt.figure(figsize=(10,10))
+    f.tight_layout()
+
+    # drawing the network by style
+    if plot_style == 'circular':
+        nx.draw_circular(G, with_labels=True, width = weight,
+                         alpha = 0.9, node_size = node_sizes,
+                         node_color = 'lightgrey', font_size=10,
+                         edgecolors = 'blue')
+    
+    elif plot_style == 'kamada_kawai':
+        nx.draw_kamada_kawai(G, with_labels=True, width = weight,
+                             alpha = 0.9, node_size = node_sizes,
+                             node_color = 'lightgrey', font_size=10,
+                             edgecolors = 'blue')
+        
+    elif plot_style == 'spring': # default
+        nx.draw_spring(G, with_labels=True, width = weight,
+                         alpha = 0.9, node_size = node_sizes,
+                         node_color = 'lightgrey', font_size=10,
+                         edgecolors = 'blue')
+    
+    elif plot_style == 'random':
+        nx.draw_random(G, with_labels=True, width = weight,
+                         alpha = 0.9, node_size = node_sizes,
+                         node_color = 'lightgrey', font_size=10,
+                         edgecolors = 'blue')
+        
+    # saving the plot
+    outpath = os.path.join('output', f'{file_name}_{plot_style}_{n}_{e}.png')
+    plt.savefig(outpath, bbox_inches='tight')
+    return
+
+
 def network_csv(dg, ev, bc, file_name):
     '''
-    function that creates a CSV file for a network
+    Function that creates a CSV file for a network containing centrality measures.
     
     dg: dict of degree centrality scores
     ev: dict of eigen vector centrality scores
@@ -84,43 +168,12 @@ def network_csv(dg, ev, bc, file_name):
     df_network = pd.DataFrame.from_dict(dd, orient = 'index', columns = ['Degree', 'Eigenvector','Betweenness'])
     df_network = df_network.rename_axis("Name").reset_index()
     
-    outpath = os.path.join('output', f'network_{file_name}.csv')
+    outpath = os.path.join('output', f'{file_name}_network.csv')
     df_network.to_csv(outpath, index = False)
     
     return
  
- 
-def plot_network(G, file_name):
-    '''
-    Function that plots a network graph in different styles and saves it with the filename the graph was created from.
     
-    G: a networkx graph object
-    file_name: the filename we are working with
-    '''
-    # grabbing plot argument
-    args = parse_args()
-    plot_style = args['plot_style']
-    
-    # drawing the network by style
-    if plot_style == 'circular':
-        nx.draw_circular(G, with_labels=True, node_size=20, font_size=10)
-    
-    elif plot_style == 'kamada_kawai':
-        nx.draw_kamada_kawai(G, with_labels=True, node_size=20, font_size=10)
-        
-    elif plot_style == 'spring':
-        nx.draw_spring(G, with_labels=True, node_size=20, font_size=10)
-    
-    elif plot_style == 'standard':
-        nx.draw_networkx(G, with_labels=True, node_size=20, font_size=10)
-        
-    # saving the plot
-    outpath = os.path.join('output', f'nx_{plot_style}_{file_name}.png')
-    plt.savefig(outpath, dpi=300, bbox_inches='tight')
-        
-    return
-
-
 def main():
     '''
     The process of the script.
@@ -136,7 +189,7 @@ def main():
         df, filename = read_df(input_name)
         G, dg, ev, bc = network_analysis(df)
         network_csv(dg, ev, bc, filename)
-        plot_network(G, filename)
+        plot_network(G, df, filename)
         
         print('[INFO] Script success.')
 
@@ -151,11 +204,10 @@ def main():
             df, filename = read_df(file)
             G, dg, ev, bc = network_analysis(df)
             network_csv(dg, ev, bc, filename)
-            plot_network(G, filename)
+            plot_network(G, df, filename)
         
         print('[INFO] Script success.')
                 
 
 if __name__ == '__main__':
     main()
-
